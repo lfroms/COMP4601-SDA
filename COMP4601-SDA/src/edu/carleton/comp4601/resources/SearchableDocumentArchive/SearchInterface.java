ac32bfc32c8da8ff7191d55f3afb657ddfb2b5f7
Lukas Romsicki <lukas@romsicki.com>
2020-02-20 15:15:19 -0500

    Add search server functionality

---
+++ COMP4601-SDA/src/edu/carleton/comp4601/resources/SearchableDocumentArchive/SearchInterface.java
@@ -0,0 +1,97 @@
+package edu.carleton.comp4601.resources.SearchableDocumentArchive;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import edu.carleton.comp4601.dao.Document;
+import edu.carleton.comp4601.dao.DocumentCollection;
+import edu.carleton.comp4601.store.DataCoordinator;
+
+final class SearchInterface {
+	private final static DataCoordinator coordinator = DataCoordinator.getInstance();
+	
+	public final static DocumentCollection searchLocalXML(String terms) {
+		return coordinator.search(terms);
+	}
+	
+	public final static DocumentCollection searchDistributedXML(String terms) {
+		return coordinator.searchDistributed(terms);
+	}
+
+	public final static String searchLocalHTML(String terms) {
+		DocumentCollection collection = coordinator.search(terms);
+		
+		return convertCollectionToHTML(collection);
+	}
+
+	public final static String searchLocalJSON(String terms) {
+		DocumentCollection collection = coordinator.search(terms);
+	
+		return convertCollectionToJSON(collection);
+	}
+	
+	public final static String searchDistributedHTML(String terms) {
+		DocumentCollection collection = coordinator.searchDistributed(terms);
+		
+		return convertCollectionToHTML(collection);
+	}
+
+	public final static String searchDistributedJSON(String terms) {
+		DocumentCollection collection = coordinator.searchDistributed(terms);
+	
+		return convertCollectionToJSON(collection);
+	}
+	
+	// PRIVATE HELPERS ==================================================================
+	
+	private final static String convertCollectionToHTML(DocumentCollection collection) {
+		String output = "<html><body>";
+		output += "<table>";
+		output += "<tr>";
+		output += "<th>ID</th>";
+		output += "<th>URL</th>";
+		output += "<th>Score</th>";
+		output += "</tr>";
+		
+		for (Document doc : collection.getDocuments()) {
+			output += convertDocumentToHTML(doc);
+		}
+		
+		output += "</table></body></html>";
+		
+		return output;
+	}
+	
+	private final static String convertCollectionToJSON(DocumentCollection collection) {
+		JSONArray output = new JSONArray();
+		
+		for (Document doc : collection.getDocuments()) {
+			output.put(convertDocumentToJSON(doc));
+		}
+		
+		return output.toString();
+	}
+	
+	private final static String convertDocumentToHTML(Document doc) {
+		String output = "";
+		
+		output += "<tr>";
+		output += "<td>" + doc.getId() + "</td>";
+		output += "<td><a href=\"" + doc.getUrl() + "\">" + doc.getUrl() + "</td>";
+		output += "<td>" + doc.getScore() + "</td>";
+		output += "</tr>";
+
+		return output;
+	}
+	
+	private final static JSONObject convertDocumentToJSON(Document doc) {
+		JSONObject output = new JSONObject();
+		
+		output
+			.put("id", doc.getId())
+			.put("url", doc.getUrl())
+			.put("score", doc.getScore());
+		
+		return output;
+	}
+}
--- COMP4601-SDA/src/edu/carleton/comp4601/resources/SearchableDocumentArchive/SearchResource.java
+++ COMP4601-SDA/src/edu/carleton/comp4601/resources/SearchableDocumentArchive/SearchResource.java
@@ -2,32 +2,32 @@ package edu.carleton.comp4601.resources.SearchableDocumentArchive;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("sda/search/{terms}")
 public class SearchResource {
+	private final static String PARAM_NAME = "terms";
 	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String getHTML() {
-		// TODO Return HTML
-		return "";
+	public String getHTML(@PathParam(PARAM_NAME) String terms) {
+		return SearchInterface.searchLocalHTML(terms);
 	}
 	
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public String getXML() {
-		// TODO Return XML
-		return "";
+	public Response getXML(@PathParam(PARAM_NAME) String terms) {
+		return Response.ok(SearchInterface.searchLocalXML(terms), MediaType.APPLICATION_XML).build();
 	}
 	
 	// For client application.
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getJSON() {
-		// TODO Return JSON
-		return "";
+	public String getJSON(@PathParam(PARAM_NAME) String terms) {
+		return SearchInterface.searchLocalJSON(terms);
 	}
 	
 }
