2f9bc2a1c3a56bc57c76a33513584d8bcc16a2c0
Lukas Romsicki <lukas@romsicki.com>
2020-02-20 13:29:05 -0500

    Add temporary logic to DocumentResource

--- COMP4601-SDA/src/edu/carleton/comp4601/resources/SearchableDocumentArchive/DocumentResource.java
+++ COMP4601-SDA/src/edu/carleton/comp4601/resources/SearchableDocumentArchive/DocumentResource.java
@@ -1,18 +1,37 @@
 package edu.carleton.comp4601.resources.SearchableDocumentArchive;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import edu.carleton.comp4601.store.DataCoordinator;
+
+
 @Path("sda/{id: [0-9]*}")
 public class DocumentResource {
+	private final static String PARAM_NAME = "id";
+	
+	private final DataCoordinator coordinator = DataCoordinator.getInstance();
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String get(@PathParam("id") String id) {
-		return "";
+	public String get(@PathParam(PARAM_NAME) String id) {
+		// TODO Need to run this through DocumentInterface when accessing coordinator and transforming.
+		return coordinator.find(mapId(id)).get().toJSON().toString();
+	}
+	
+	@DELETE
+	public void delete(@PathParam(PARAM_NAME) String id) {
+		coordinator.delete(mapId(id));
+	}
+	
+	// PRIVATE HELPERS ==================================================================
+	
+	private static final Integer mapId(String id) {
+		return Integer.valueOf(id);
 	}
 
 }
