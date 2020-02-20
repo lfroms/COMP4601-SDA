b193ee83110fe32c97bfdd0b22486fb6adc4c8c2
Lukas Romsicki <lukas@romsicki.com>
2020-02-20 15:17:35 -0500

    Add resource for distributed search

--- COMP4601-SDA/src/edu/carleton/comp4601/resources/SearchableDocumentArchive/QueryResource.java
+++ COMP4601-SDA/src/edu/carleton/comp4601/resources/SearchableDocumentArchive/QueryResource.java
@@ -7,27 +7,27 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("sda/search/{terms}")
-public class SearchResource {
+@Path("sda/query/{terms}")
+public class QueryResource {
 	private final static String PARAM_NAME = "terms";
 	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String getHTML(@PathParam(PARAM_NAME) String terms) {
-		return SearchInterface.searchDistributedHTML(terms);
+		return SearchInterface.searchLocalHTML(terms);
 	}
 	
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
 	public Response getXML(@PathParam(PARAM_NAME) String terms) {
-		return Response.ok(SearchInterface.searchDistributedXML(terms), MediaType.APPLICATION_XML).build();
+		return Response.ok(SearchInterface.searchLocalXML(terms), MediaType.APPLICATION_XML).build();
 	}
 	
 	// For client application.
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getJSON(@PathParam(PARAM_NAME) String terms) {
-		return SearchInterface.searchDistributedJSON(terms);
+		return SearchInterface.searchLocalJSON(terms);
 	}
 	
 }
