6f013741903594d7eb5c3422ca3c516f8a56c4b8
Lukas Romsicki <lukas@romsicki.com>
2020-02-21 14:09:42 -0500

    Update Lucene field names to match spec

--- COMP4601-SDA/src/edu/carleton/comp4601/store/index/IndexDocumentFields.java
+++ COMP4601-SDA/src/edu/carleton/comp4601/store/index/IndexDocumentFields.java
@@ -1,12 +1,12 @@
 package edu.carleton.comp4601.store.index;
 
 public final class IndexDocumentFields {
-	public final static String ID = "id";
+	public final static String ID = "docId";
 	public final static String TITLE = "title";
-	public final static String INDEXED_BY = "indexed_by";
+	public final static String INDEXED_BY = "i";
 	public final static String URL = "url";
 	public final static String DATE = "date";
 	public final static String CONTENT = "content";
-	public final static String IMAGE_ALT_TEXTS = "image_alt_texts";
+	public final static String IMAGE_ALT_TEXTS = "imageAltTexts";
 	public final static String TYPE = "type";
 }
