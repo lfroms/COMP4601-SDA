5d4432738e0d38d68801051d676b212e8bb9d4f4
Lukas Romsicki <lukas@romsicki.com>
2020-02-21 19:00:24 -0500

    Adjust DocumentResource so it only matches numerical IDs

--- COMP4601-SDA/src/edu/carleton/comp4601/resources/SearchableDocumentArchive/DocumentResource.java
+++ COMP4601-SDA/src/edu/carleton/comp4601/resources/SearchableDocumentArchive/DocumentResource.java
@@ -7,7 +7,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("sda/{id}")
+@Path("sda/{id : \\d+}")
 public class DocumentResource {
 	private final static String PARAM_NAME = "id";
 	
