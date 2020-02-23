43c9a7006be9d31f5ffa2dd82c9e876b2ea932b1
Lukas Romsicki <lukas@romsicki.com>
2020-02-22 19:49:55 -0500

    Add null check for scores

--- COMP4601-SDA/src/edu/carleton/comp4601/helpers/DocumentScoreComparator.java
+++ COMP4601-SDA/src/edu/carleton/comp4601/helpers/DocumentScoreComparator.java
@@ -8,6 +8,10 @@ public final class DocumentScoreComparator implements Comparator<Document> {
 
 	@Override
 	public int compare(Document o1, Document o2) {
+		if (o1.getScore() == null || o2.getScore() == null) {
+			return 0;
+		}
+		
 		return o2.getScore().compareTo(o1.getScore());
 	}
 
